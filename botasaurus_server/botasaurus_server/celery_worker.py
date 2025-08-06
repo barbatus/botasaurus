e9bffca2eb30ad07308ead826fb6018e4b4d5611
alex <alex.borodach@gmail.com>
2025-08-06 23:27:56 +0200

    Change Celery worker config

--- botasaurus_server/botasaurus_server/celery_worker.py
+++ botasaurus_server/botasaurus_server/celery_worker.py
@@ -1,7 +1,5 @@
 from celery import Celery
 
-from .server import Server
-
 celery_app = Celery(
     "worker",
     broker="redis://host.docker.internal:6379/0",
@@ -9,12 +7,15 @@ celery_app = Celery(
 )
 
 celery_app.conf.update(
-    worker_concurrency=int(Server.rate_limit["browser"]),
+    worker_concurrency=5,
     worker_prefetch_multiplier=1,
     task_always_eager=False,
     task_ignore_result=False,
-    task_soft_time_limit=3600,
-    task_time_limit=3900,
-    worker_pool="threads",
+    task_acks_late=True,
+    task_reject_on_worker_lost=True,
+    task_soft_time_limit=840,
+    task_time_limit=900,
+    worker_pool="prefork",
     result_expires=3600,
+    max_tasks_per_child=50,
 )
