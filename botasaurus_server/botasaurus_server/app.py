d43801ef8dd679bed2bd3614dc6a4d9a9136d460
alex <alex.borodach@gmail.com>
2025-08-20 20:52:00 +0200

    Imports tasks in the celery worker

--- botasaurus_server/botasaurus_server/app.py
+++ botasaurus_server/botasaurus_server/app.py
@@ -1,10 +1,8 @@
-# from .env import is_in_kubernetes
+from .fastapi_app import app as fastapi_app
 
 
 def run_server():
     import uvicorn
-
-    from .fastapi_app import app as fastapi_app
 
     uvicorn.run(fastapi_app, host="0.0.0.0", port=8000, log_level="info")
 
